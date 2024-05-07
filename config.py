5c491656e1529c707aee589431e2fe171d1ac3ac
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-05-08 01:28:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = "6668216408:AAEToeZhZ_se3H18s1eFglvvh1TqYoUwN-Q"
 STRING_SESSION = "AQGFxnsAoOMJt4OIPmxzXe5f8m7OYf5GMlgxFclxELVDSdwDtIO1QkGSI4KBXqMgsF82jQoDaSAXDhxdizydlx19qzS3hK5KHjZxWM_dqm-GWfPkDCw2MpKcSh-Pgp5IhRm4WaxXB5_RLmeEVC3OpnP_bYYfmhmrkH8RvpbqWRppbs98n0sF7mIBIzmdL9EId4d5QXu264nyjVJCLyUnQuaJK-4NwFTp-vDO63GCfrdbcjE4Lm0uaKuo0y1Hp5nQRWELyDCFsfv_qPjJcI5VMYyNVseZoD-VbYzQKMpWFTnYk9ELCPwH5GkKUY8PZeQIqEASLdcQp-KFxJJ5s2ict8VBGxL9rwAAAAFFOpN6AA"
 OWNER_ID = "5529497172"
 ADMINS = [949657126]
-BASE_URL = "https://movieloverzzstreaming-37788a19d54d.herokuapp.com/channel/hsjsbwjoeo"
+BASE_URL = "https://movieloverzzstreaming-37788a19d54d.herokuapp.com/"
 HOME_PAGE_REDIRECT = "https://t.me/hsjsbwjoeo"
